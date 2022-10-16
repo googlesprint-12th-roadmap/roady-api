a93b30cdb3e0acd5788967831458ca0e4995bb8b
devel <develop.raul@gmail.com>
2022-10-16 21:47:52 +0900

    [ADD] add cors settings

--- src/main/kotlin/com/roady/roadyapi/roadmap/adaptor/input/web/controller/RoadmapController.kt
+++ src/main/kotlin/com/roady/roadyapi/roadmap/adaptor/input/web/controller/RoadmapController.kt
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 @RequestMapping("/api/v1/roadmap")
 class RoadmapController(
     private val roadmapProperty: RoadmapProperty,
