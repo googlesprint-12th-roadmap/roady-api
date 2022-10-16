be76a4a7f64c14dbb88db72ff69efd2f68f1f9bc
devel <develop.raul@gmail.com>
2022-10-16 19:01:26 +0900

    [ADD] add entities

---
+++ src/main/kotlin/com/roady/roadyapi/roadmap/adaptor/output/persistence/data/entity/RoadmapEntity.kt
@@ -0,0 +1,12 @@
+package com.roady.roadyapi.roadmap.adaptor.output.persistence.data.entity
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "roadmap")
+class RoadmapEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idx: Long = 0,
+    val ownerIdx: Long,
+    val name: String
+)
