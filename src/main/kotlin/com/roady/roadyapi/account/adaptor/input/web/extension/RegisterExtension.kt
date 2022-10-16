aaf57818ffa9ba9243181e1ed51cc40491675216
devel <develop.raul@gmail.com>
2022-10-16 15:38:04 +0900

    [ADD] add low-level layer

--- src/main/kotlin/com/roady/roadyapi/account/adaptor/input/web/extension/RegisterExtension.kt
+++ src/main/kotlin/com/roady/roadyapi/account/adaptor/input/web/extension/RegisterExtension.kt
@@ -1,10 +1,17 @@
 package com.roady.roadyapi.account.adaptor.input.web.extension
 
+import com.roady.roadyapi.account.adaptor.input.web.data.request.LoginRequest
 import com.roady.roadyapi.account.adaptor.input.web.data.request.RegisterRequest
+import com.roady.roadyapi.account.domain.Login
 import com.roady.roadyapi.account.domain.Register
 
 fun RegisterRequest.toDomain() = Register(
     nickname = nickname,
     id = id,
     password = password
+)
+
+fun LoginRequest.toDomain() = Login(
+    id = id,
+    rawPassword = password
 )
