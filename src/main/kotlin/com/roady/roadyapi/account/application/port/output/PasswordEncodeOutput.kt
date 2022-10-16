0c67b49f54c6285a29b1dcf217c70abb06e1f567
devel <develop.raul@gmail.com>
2022-10-16 15:37:46 +0900

    [ADD] add and implement password encode output spec

--- src/main/kotlin/com/roady/roadyapi/account/application/port/output/PasswordEncodeOutput.kt
+++ src/main/kotlin/com/roady/roadyapi/account/application/port/output/PasswordEncodeOutput.kt
@@ -2,4 +2,5 @@ package com.roady.roadyapi.account.application.port.output
 
 interface PasswordEncodeOutput {
     fun encode(rawPassword: String): String
+    fun matches(rawPassword: String, encodedPassword: String): Boolean
 }
